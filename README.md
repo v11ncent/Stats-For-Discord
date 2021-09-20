c3b091551520678852729a103119e88fd36f846f
Vince <45806875+vince1444@users.noreply.github.com>
2021-09-19 21:53:05 -0400

    First commit; added basic file structure and dependencies

---
+++ README.md
@@ -0,0 +1,8 @@
+# Objectives
+Track & congregate all songs listened to by users in a Discord server and display on a website
+
+# Tech stack
+MERN
+
+# Contributors
+Me.
---
+++ backend/main.js
@@ -0,0 +1,18 @@
+/*
+=================================================
+Last change(s): 9/19/2021
+
+Objectives:
+
+1. Fetch all songs from users in a Discord server
+
+2. Create noSQL database and tie songs to users
+
+3. Congregate all tracks & artists by month/6 months/all time
+
+4. Display on website
+
+=================================================
+*/
+
+console.log('Hello, world...');
